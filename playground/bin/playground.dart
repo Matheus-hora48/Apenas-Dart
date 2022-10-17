5b569bcd7924bd2c40f773f5260fd4f456204377
Matheus Hora <matheus11hora@gmail.com>
2022-10-17 12:32:09 -0300

    Novos

--- playground/bin/playground.dart
+++ playground/bin/playground.dart
@@ -1,5 +1,5 @@
 import 'package:playground/playground.dart' as playground;
 
-void main(List<String> arguments) {
-  print('Hello world: ${playground.calculate()}!');
+void main() {
+  print(playground.fibonacciTwo(5));
 }
--- playground/lib/playground.dart
+++ playground/lib/playground.dart
@@ -1,3 +1,29 @@
-int calculate() {
-  return 6 * 7;
+List<int> sequencia = [];
+var index = 0;
+var limite = 100;
+
+List<int> fibonacci(int elementy, int tamanho) {
+  if (sequencia.isEmpty) {
+    sequencia.add(elementy);
+  } else if (sequencia.length == 1) {
+    sequencia.add(elementy + 1);
+  } else {
+    sequencia.add(sequencia[index] + sequencia[index + 1]);
+    index++;
+  }
+  if (sequencia.length >= tamanho) return sequencia;
+  return fibonacci(sequencia.last, tamanho);
+}
+
+List<int> fibonacciTwo(int tamanho) {
+  if (sequencia.isEmpty) {
+    sequencia.add(0);
+  } else if (sequencia.length == 1) {
+    sequencia.add(0 + 1);
+  } else {
+    sequencia.add(sequencia[index] + sequencia[index + 1]);
+    index++;
+  }
+  if (sequencia.length >= tamanho) return sequencia;
+  return fibonacciTwo(tamanho);
 }
