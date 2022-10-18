c7a404087dae78a90f477c70045ca4c5c94e33ad
Matheus Hora <matheus11hora@gmail.com>
2022-10-17 23:35:12 -0300

    Novos

--- playground/bin/playground.dart
+++ playground/bin/playground.dart
@@ -2,4 +2,5 @@ import 'package:playground/playground.dart' as playground;
 
 void main() {
   print(playground.fibonacciTwo(15));
+  print(playground.regraDeTres(posicao11: 15,posicao12: 10, posicao21: 8));
 }
--- playground/lib/playground.dart
+++ playground/lib/playground.dart
@@ -8,11 +8,32 @@ List<int> fibonacciTwo(int tamanho) {
   } else if (sequencia.length == 1) {
     sequencia.add(1);
   } else {
-    sequencia.add((sequencia[index] + sequencia[index -1]));
+    sequencia.add((sequencia[index] + sequencia[index - 1]));
     index++;
   }
   if (sequencia.length >= tamanho) return sequencia;
   return fibonacciTwo(tamanho);
 }
 
+double regraDeTres({
+  double posicao11 = 0,
+  double posicao12 = 0,
+  double posicao21 = 0,
+  double posicao22 = 0,
+}) {
+  var x = [posicao11, posicao12, posicao21, posicao22];
+  double value = 0.0;
 
+  if (x[0] == 0.0) {
+    value = (posicao12 * posicao21) / posicao22;
+  } else if (x[1] == 0.0) {
+    value = (posicao11 * posicao22) / posicao21;
+  } else if (x[2] == 0.0) {
+    value = (posicao11 * posicao22) / posicao12;
+  } else if (x[3] == 0.0) {
+    value = (posicao21 * posicao12) / posicao11;
+  } else {
+    print('Nenhum valor nulo!');
+  }
+  return value;
+}
